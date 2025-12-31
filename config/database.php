516fbdbd199a51c48d88d3c9bac9e6552801f456
tsukiyasan <zeronored@gmail.com>
2025-12-31 14:55:55 +0800

    新增pgsql設定檔

--- config/database.php
+++ config/database.php
@@ -78,6 +78,21 @@ return [
             'server_version' => env('DB_ORACLE_SERVER_VERSION', '11g'),
         ],
 
+        'pgsql' => [
+                'driver' => 'pgsql',
+                'url' => env('DATABASE_URL'),
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '5432'),
+                'database' => env('DB_DATABASE', 'forge'),
+                'username' => env('DB_USERNAME', 'forge'),
+                'password' => env('DB_PASSWORD', ''),
+                'charset' => 'utf8',
+                'prefix' => '',
+                'prefix_indexes' => true,
+                'search_path' => 'public',
+                'sslmode' => 'prefer',
+            ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
