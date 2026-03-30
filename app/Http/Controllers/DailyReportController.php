661ab047571ff4dbcc6b0f53410a96f1e60ba8f6
tsukiyasan <zeronored@gmail.com>
2026-03-30 08:44:28 +0800

    修正錯誤

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -89,7 +89,7 @@ class DailyReportController extends Controller
             ->appends($request->all());
 
         // 7. 回傳 View
-        return view('production_daily', [
+        return view('production_daily_report', [
             'plans'     => $plans,
             'lines'     => $lines,
             'dateStart' => $dateStartInput, // 回傳給前端顯示用 (Y-m-d)
