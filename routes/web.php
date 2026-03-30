2f9b2225889e2f4b5bca22ad70e03a8501c908b7
tsukiyasan <zeronored@gmail.com>
2026-03-30 08:40:36 +0800

    補上router

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductionPlanController;
+use App\Http\Controllers\DailyReportController;
 Route::get('/', function () {
     return view('welcome');
 });
