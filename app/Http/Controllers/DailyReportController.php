514f6d43ef2c5a2e44dc1e915beda811ac541649
tsukiyasan <zeronored@gmail.com>
2026-04-02 13:33:10 +0800

    修正錯誤

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class ProductionPlanController extends Controller
+class DailyReportController extends Controller
 {
     public function index(Request $request)
     {
