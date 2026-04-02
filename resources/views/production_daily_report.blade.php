b4060a59b380253fc7988b17e4596c0faac9fc33
tsukiyasan <zeronored@gmail.com>
2026-04-02 14:56:05 +0800

    改成浮點數

--- resources/views/production_daily_report.blade.php
+++ resources/views/production_daily_report.blade.php
@@ -125,7 +125,7 @@
                                 {{ number_format($row->tonyu_su) }}
                             </td>
                             <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{{ $row->keitai_name }}</td>
-                            <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{{ number_format($row->itaatsu) }}</td>
+                            <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{{ number_format((float)$row->itaatsu, 1) }}</td>
                             <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-500">{{ $row->order_no }}</td>
                         </tr>l
                     @empty
